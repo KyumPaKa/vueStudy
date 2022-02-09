0f105e95b02c7e74734a79824a1e1dd8675b8603
ckm <wras456@naver.com>
2022-02-09 10:34:46 +0900

    basic

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- HTML -->
+    <div>header</div>
+</template>
+
+<script>
+export default {
+    // Javascript - 인스턴스 옵션
+    method: {
+        addNum: function() {
+
+        }
+    }
+}
+</script>
+
+<style>
+    /* css */
+</style>
