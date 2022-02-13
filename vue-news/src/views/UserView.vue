109c72599e1d3f5d391cde6fa7fdcf34e0e8c9a4
ckm <wras456@naver.com>
2022-02-13 14:44:51 +0900

    news/6-userComponent

--- vue-news/src/views/UserView.vue
+++ vue-news/src/views/UserView.vue
@@ -1,14 +1,20 @@
 <template>
     <div>
-        <p>name : {{ this.userInfo.id }}</p>
-        <p>karma : {{ this.userInfo.karma }}</p>
-        <p>created: {{ this.userInfo.created }}</p>
-        <p>about: {{ this.userInfo.about }}</p>
+        <user-profile :info="userInfo">
+            <div slot="username">{{ userInfo.id }}</div>
+            <span slot="time">{{ 'Joined ' + userInfo.created }}, </span>
+            <span slot="karma">{{ userInfo.karma }}</span>
+        </user-profile>
     </div>
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile.vue';
+
 export default {
+    components: {
+        UserProfile
+    },
     computed: {
         userInfo() {
             return this.$store.state.user;
@@ -16,7 +22,6 @@ export default {
     },
     created() {
         const userName = this.$route.params.id;
-        // axios.get(`https://api.hnpwa.com/v0/user/${userName}.json`);
         this.$store.dispatch('FETCH_USER', userName);
     }
 }
