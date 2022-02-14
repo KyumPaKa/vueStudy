557687744908ac4150ec76968516d060ed5a3544
ckm <wras456@naver.com>
2022-02-14 10:25:53 +0900

    news/8-routerNavi

--- vue-news/src/routes/index.js
+++ vue-news/src/routes/index.js
@@ -6,6 +6,8 @@ import JobsView from '../views/JobsView.vue';
 import UserView from '../views/UserView.vue';
 import ItemView from '../views/ItemView.vue';
 import createListView from '../views/CreateListView.js';
+import bus from '../utils/bus.js';
+import { store } from '../store/index.js';
 
 Vue.use(VueRouter);
 
@@ -21,20 +23,59 @@ export const router = new VueRouter({
             path: '/news',
             name: 'news',
             // component: url 주소로 갔을 때 표시될 컴포넌트
-            component: NewsView
+            component: NewsView,
             // component: createListView('NewsView')
+            beforeEnter: (to, from, next) => {
+                bus.$emit('start:spinner');
+                store.dispatch('FETCH_LIST', to.name)
+                    .then(() => {
+                        // console.log('fetched');
+                        // bus.$emit('end:spinner');
+                        next();
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+                // console.log('to', to);
+                // console.log('from', from);
+                // console.log('next', next);
+            }
         },
         {
             path: '/ask',
             name: 'ask',
-            component: AskView
+            component: AskView,
             // component: createListView('AskView')
+            beforeEnter: (to, from, next) => {
+                bus.$emit('start:spinner');
+                store.dispatch('FETCH_LIST', to.name)
+                    .then(() => {
+                        // console.log('fetched');
+                        // bus.$emit('end:spinner');
+                        next();
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
         },
         {
             path: '/jobs',
             name: 'jobs',
-            component: JobsView
+            component: JobsView,
             // component: createListView('JobsView')
+            beforeEnter: (to, from, next) => {
+                bus.$emit('start:spinner');
+                store.dispatch('FETCH_LIST', to.name)
+                    .then(() => {
+                        // console.log('fetched');
+                        // bus.$emit('end:spinner');
+                        next();
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
         },
         {
             path: '/user/:id',
--- vue-news/src/store/actions.js
+++ vue-news/src/store/actions.js
@@ -2,7 +2,7 @@ import { fetchNewsList, fetchJobsList, fetchAskList, fetchUserInfo, fetchComment
 
 export default {
     FETCH_NEWS(context) {
-        fetchNewsList()
+        return fetchNewsList()
         .then(response => {
             context.commit('SET_NEWS', response.data)
             return response;
@@ -10,30 +10,32 @@ export default {
         .catch(error => console.log(error));
     },
     FETCH_JOBS({ commit }) {
-        fetchJobsList()
+        return fetchJobsList()
             .then(({ data }) => {
                 commit('SET_JOBS', data)
             })
             .catch(error => console.log(error));
     },
     FETCH_ASK({ commit }) {
-        fetchAskList()
+        return fetchAskList()
             .then(({ data }) => commit('SET_ASK', data))
             .catch(error => console.log(error));
     },
     FETCH_USER({ commit }, name) {
-        fetchUserInfo(name)
+        return fetchUserInfo(name)
             .then(({ data }) => commit('SET_USER', data))
             .catch(error => console.log(error));
     },
     FETCH_ITEM({ commit }, id) {
-        fetchCommentItem(id)
+        return fetchCommentItem(id)
             .then(({ data }) => commit('SET_ITEM', data))
             .catch(error => console.log(error));
     },
     FETCH_LIST({ commit }, pageName){
-        fetchList(pageName)
-            .then(({ data }) => commit('SET_LIST', data))
+        return fetchList(pageName)
+            .then(({ data }) => {
+                commit('SET_LIST', data)
+            })
             .catch(error => console.log(error));
     }
 };
