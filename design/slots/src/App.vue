a0d0651e2d44cd6f11c8cdf1c0d3d427f97d18b5
ckm <wras456@naver.com>
2022-02-14 15:51:31 +0900

    news/11-designPattern

---
+++ design/slots/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <ul>
+            <item>
+                아이템1
+            </item>
+            <item>
+                아이템2
+                <button>click me</button>
+            </item>
+            <item>
+                <div>
+                    아이템3
+                </div>
+                <img src="" alt="이미지" width="50px;" height="50px;">
+            </item>
+            <item>
+                <div style="color: blue; font-size: 20px;">아이템4</div>
+            </item>
+      </ul>
+  </div>
+</template>
+
+<script>
+import Item from './components/Item.vue';
+
+export default {
+    components: {
+        Item
+    }
+}
+</script>
