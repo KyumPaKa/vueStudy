3646efb262984514900ce88ce9bc6c6a7ef14f7b
ckm <wras456@naver.com>
2022-02-14 14:02:25 +0900

    news/10-exlib

---
+++ chart-lib/src/components/BarChart.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <canvas ref="barChart" id="barChart" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['propsdata'],
+  // 컴포넌트 속성 && 인스턴스 옵션
+  mounted() {
+    // const ctx = document.getElementById('barChart');
+    const ctx = this.$refs.barChart;
+    const myChart = new this.$_Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: this.propsdata
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+  }
+}
+</script>
+
+<style>
+
+</style>
